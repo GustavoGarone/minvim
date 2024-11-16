4352884bdcf32fec70a8b56d83c412223a212db9
Minze <gustavogarone@protonmail.com>
2024-11-16 20:41:27 -0300

    generics opts

--- config/opts.nix
+++ config/opts.nix
@@ -1,5 +1,13 @@
 {
   opts = {
+    number = true;
     relativenumber = true;
+    expandtab = true;
+    shiftwidth = 4;
+    tabstop = 4;
+    smarttab = true;
+    cursorline = true;
+    undolevels = 500;
+    history = 500;
   };
 }
