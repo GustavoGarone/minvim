106faaebf332eecd552e728bfd64d713d7089457
Minze <gustavogarone@protonmail.com>
2024-11-18 10:00:33 -0300

    neo-tree

--- config/uis.nix
+++ config/uis.nix
@@ -3,6 +3,9 @@
     bufferline.enable = true;
     web-devicons.enable = true;
     lualine.enable = true;
+    neo-tree = {
+      enable = true;
+    };
   };
   colorscheme = "catppuccin";
   colorschemes.catppuccin.enable = true;
