d93d60322570da66030d14f8f03692c896601153
Minze <gustavogarone@protonmail.com>
2025-01-25 15:24:58 -0300

    everforest after issue fix

--- config/uis.nix
+++ config/uis.nix
@@ -10,13 +10,13 @@
     };
   };
   # colorscheme = "everforest";
-  colorschemes.monokai-pro = {
+  colorschemes.everforest = {
     enable = true;
-    settings = {
-      devicons = true;
-      day_night.enable = true;
-      day_night.day_filter = "spectrum";
-      day_night.night_filter = "soda";
-    };
+    # settings = {
+    #   devicons = true;
+    #   day_night.enable = true;
+    #   day_night.day_filter = "spectrum";
+    #   day_night.night_filter = "soda";
+    # };
   };
 }
